4e370266e1227ecaa5070576c53d6683cfc0e09e
joekrush <42564550+joekrush@users.noreply.github.com>
2018-09-07 18:57:09 -0500

    initialized at i=-1 to split square and fixed spelling of modulus

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -37,9 +37,9 @@ int isKaprekar(int n) {
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=-1; i<=numberOfDigits; i++) {
     //increase the modulus by a factor of 10
-    long modulous = modulous * 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
